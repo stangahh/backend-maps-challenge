82176e0354d70ad77b99ba8e01a1f7ff0682ab8c
Jesse Stanger <jesse.stanger@gmail.com>
2021-03-13 16:53:50 +1000

    refactor: move helper to own file and class
    add new addresses class for higher order functionality

--- src/addresses.spec.ts
+++ src/addresses.spec.ts
@@ -1,49 +1,19 @@
-import { MapsAPIHelper } from './addresses'
+import { Addresses } from './addresses'
 
 const validKey = process.env.MAPS_API_KEY ?? 'SOME_VALID_KEY'
 
-describe('MapsAPIHelper', () => {
-  let mapsApiHelper: MapsAPIHelper
+describe('Addresses', () => {
+  let addresses: Addresses
 
   beforeEach(() => {
-    mapsApiHelper = new MapsAPIHelper(validKey)
+    addresses = new Addresses(validKey)
   })
 
   it('should create when valid key', () => {
-    expect(mapsApiHelper).toBeTruthy()
+    expect(addresses).toBeTruthy()
   })
 
   it('should not create when empty key', () => {
-    expect(() => new MapsAPIHelper('')).toThrowError()
-  })
-
-  // Can't really test whether a key is actually capable of calling the API until we try calling it
-  // We'll handle that in the integration tests.
-})
-
-// Integration tests
-describe('MapsAPIHelper get places and place data', () => {
-  let mapsApiHelper: MapsAPIHelper
-
-  beforeEach(() => {
-    mapsApiHelper = new MapsAPIHelper(validKey)
-  })
-
-  it('should get places', async () => {
-    const places = await mapsApiHelper.getPlaces('Brisbane')
-    expect(places).toBeTruthy()
-  })
-
-  it('should not get places when empty string provided', async () => {
-    await expect(mapsApiHelper.getPlaces('')).rejects.toThrowError()
-  })
-
-  it('should get place data from a place id', async () => {
-    const places = await mapsApiHelper.getPlaces('Brisbane')
-    const placeId = places.candidates[0].place_id ?? 'ChIJM9KTrJpXkWsRQK_e81qjAgQ'
-    const placeData = await mapsApiHelper.getPlaceData(placeId)
-    expect(placeData).toBeTruthy()
+    expect(() => new Addresses('')).toThrowError()
   })
 })
-
-// Unit tests
--- src/addresses.ts
+++ src/addresses.ts
@@ -1,56 +1,18 @@
-import {
-  AddressComponent,
-  Client,
-  PlaceInputType
-} from '@googlemaps/google-maps-services-js'
-import { PlaceDetailsResponseData } from '@googlemaps/google-maps-services-js/dist/places/details'
-import { FindPlaceFromTextResponseData } from '@googlemaps/google-maps-services-js/dist/places/findplacefromtext'
+import { AddressComponent } from '@googlemaps/google-maps-services-js'
+import { MapsAPIHelper } from './helper'
 
 export interface Address {
   fullAddress: string | undefined
   address: AddressComponent[] | undefined
 }
 
-export class MapsAPIHelper {
-  private readonly client
-  private readonly key
+export class Addresses {
+  private readonly mapsHelper
 
   constructor (apiKey: string) {
     if (apiKey === '') {
       throw new Error(`Invalid API Key: '${apiKey}'`)
     }
-
-    this.key = apiKey
-
-    this.client = new Client({})
-  }
-
-  public async getPlaces (
-    partial: string
-  ): Promise<FindPlaceFromTextResponseData> {
-    return await this.client
-      .findPlaceFromText({
-        params: {
-          input: partial,
-          inputtype: PlaceInputType.textQuery,
-          key: this.key
-        },
-        timeout: 1000 // milliseconds
-      })
-      .then((r) => r.data)
-  }
-
-  public async getPlaceData (
-    placeId: string
-  ): Promise<PlaceDetailsResponseData> {
-    return await this.client
-      .placeDetails({
-        params: {
-          place_id: placeId,
-          key: this.key
-        },
-        timeout: 1000 // milliseconds
-      })
-      .then((r) => r.data)
+    this.mapsHelper = new MapsAPIHelper(apiKey)
   }
 }
